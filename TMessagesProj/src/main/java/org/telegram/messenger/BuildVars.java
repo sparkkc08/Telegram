1bedee3b95b7957b2e45224bc84aabdd363eaa81
maxim.mironyuk@gmail.com <makc14github02>
2017-06-21 00:12:39 +0300

    + Success launch of build + Added credentials for some services

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -11,12 +11,12 @@ package org.telegram.messenger;
 public class BuildVars {
     public static boolean DEBUG_VERSION = false;
     public static boolean DEBUG_PRIVATE_VERSION = false;
-    public static int BUILD_VERSION = 957;
-    public static String BUILD_VERSION_STRING = "3.18";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int BUILD_VERSION = 1;
+    public static String BUILD_VERSION_STRING = "1.0.0";
+    public static int APP_ID = 183875; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "7b2e5bf2b4173416319b08f8e84d4541"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "43ee0c3921054aa9996cdd66efae33f1";
+    public static String HOCKEY_APP_HASH_DEBUG = "be8f8510d2244658a64921acd5992fa3";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
     public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
